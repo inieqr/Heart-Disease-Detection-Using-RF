02201c4776fff7727a991e596bd7b235118971b9
inieqr <70246193+inieqr@users.noreply.github.com>
2023-04-11 18:11:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,3 +78,8 @@ st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
+
+if prediction='0'
+    st.write('You do not have heart disease')
+else
+    st.write('You have heart disease')
